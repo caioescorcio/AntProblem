7613adc8dd2464c4ab9e4be71904f2622cfb26a0
caioescorcio <caioeldourado@gmail.com>
2026-02-05 17:26:49 +0100

    🐞population class creation

---
+++ parallelisation/src/population.cpp
@@ -0,0 +1,54 @@
+#include "population.hpp"
+#include <iostream>
+#include "rand_generator.hpp"
+
+void population::advance(int index, pheronome& phen, const fractal_land& land, const position_t& pos_food, const position_t& pos_nest,
+                   std::size_t& cpteur_food ) {
+
+    auto ant_choice = [this]() mutable { return rand_double( 0., 1., this->seeds[index] ); };   // choise based on most likely pheromone route
+    auto dir_choice = [this]() mutable { return rand_int32( 1, 4, this->seeds[index] ); };      // random choice in case of no good pheromone route
+    double consumed_time = 0.;
+    while ( consumed_time < 1. ) {
+        // Si la fourmi est chargée, elle suit les phéromones de deuxième type, sinon ceux du premier.
+        int        ind_pher    = ( is_loaded( ) ? 1 : 0 );
+        double     choix       = ant_choice( );
+        position_t old_pos_ant = get_position(index);
+        position_t new_pos_ant = old_pos_ant;
+        double max_phen    = std::max( {phen( new_pos_ant.x - 1, new_pos_ant.y )[ind_pher],
+                                     phen( new_pos_ant.x + 1, new_pos_ant.y )[ind_pher],
+                                     phen( new_pos_ant.x, new_pos_ant.y - 1 )[ind_pher],
+                                     phen( new_pos_ant.x, new_pos_ant.y + 1 )[ind_pher]} );
+        if ( ( choix > m_eps ) || ( max_phen <= 0. ) ) {
+            do {
+                new_pos_ant = old_pos_ant;
+                int d = dir_choice();
+                if ( d==1 ) new_pos_ant.x  -= 1;
+                if ( d==2 ) new_pos_ant.y -= 1;
+                if ( d==3 ) new_pos_ant.x  += 1;
+                if ( d==4 ) new_pos_ant.y += 1;
+
+            } while ( phen[new_pos_ant][ind_pher] == -1 );
+        } else {
+            if ( phen( new_pos_ant.x - 1, new_pos_ant.y )[ind_pher] == max_phen )
+                new_pos_ant.x -= 1;
+            else if ( phen( new_pos_ant.x + 1, new_pos_ant.y )[ind_pher] == max_phen )
+                new_pos_ant.x += 1;
+            else if ( phen( new_pos_ant.x, new_pos_ant.y - 1 )[ind_pher] == max_phen )
+                new_pos_ant.y -= 1;
+            else  // if (phen(new_pos_ant.first,new_pos_ant.second+1)[ind_pher] == max_phen)
+                new_pos_ant.y += 1;
+        }
+        consumed_time += land( new_pos_ant.x, new_pos_ant.y);
+        phen.mark_pheronome( new_pos_ant );
+        positions[index] = new_pos_ant;
+        if ( get_position(index) == pos_nest ) {
+            if ( is_loaded(index) ) {
+                cpteur_food += 1;
+            }
+            unset_loaded(index);
+        }
+        if ( get_position(index) == pos_food ) {
+            set_loaded(index);
+        }
+    }
+}
---
+++ parallelisation/src/population.hpp
@@ -0,0 +1,61 @@
+// ant.hpp
+#ifndef _ANT_HPP_
+# define _ANT_HPP_
+# include <utility>
+# include "fractal_land.hpp"
+# include "basic_types.hpp"
+
+enum state { unloaded = 0, loaded = 1 };
+
+class Population {
+    private:
+        int m_size;
+        std::vector<position_t> positions;
+        std::vector<state> states;
+        std::vector<std::size_t> seeds;
+        double m_eps;
+    
+    public:
+        void Population(int size) : m_size(size) {
+            positions.resize(m_size);
+            states.resize(m_size, unloaded); // start all ants with unloaded state
+            seeds.resize(m_size);
+            m_eps = 0;
+        }
+
+        ~Population() = default;
+
+        void new_ant(position_t pos, state st, std::size_t seed){   // returns void because its id is not important
+            positions.push_back(pos);
+            states.push_back(st);
+            seeds.push_back(seed);
+        }
+        
+        void clone_ant(int index) {
+            new_ant(positions[index], states[index], seeds[index]);
+        }
+
+        void destroy_ant(int index) {
+            if (index >= posistions.size()) return;
+
+            positions[index] = positions.back();        // vector[index] = vector[n-1] 
+            states[index] = states.back();
+            seeds[index] = seeds.back();
+
+            positions[index] = positions.pop_back();    // vector[n-1] is popped
+            states[index] = states.pop_back();
+            seeds[index] = seeds.pop_back();
+        }
+
+        void set_loaded(int index) { states[index] = loaded; }
+        void unset_loaded(int index) { states[index] = unloaded; }
+
+        bool is_loaded(int index) const { return states[index] == loaded; }
+
+        position_t& get_position(int index) const { return positions[index]; }
+
+        static void set_exploration_coef(double eps) { m_eps = eps; }   // set commom exploration coefficient to the given value
+
+        void advance( int index, pheronome& phen, const fractal_land& land,
+                    const position_t& pos_food, const position_t& pos_nest, std::size_t& cpteur_food );
+    };
